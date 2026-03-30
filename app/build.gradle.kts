c1247a9376bb5706d028ae5955a6ec3200ed9b39
Ramazan <ramazansuleymanli@gmail.com>
2026-03-30 18:52:24 +0400

    upd: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.cloud.sync"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
